330096dff8e4474a39a14e7c160167caebc009a2
joffinjoy <joffinjoy@gmail.com>
2022-07-30 23:21:34 +0530

    Skeletal Strucutre Changed

--- app/root.jsx
+++ app/root.jsx
@@ -1,36 +1,53 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+//import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Outlet, LiveReload } from '@remix-run/react';
 
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+export default function App() {
+	return (
+		<Document>
+			<Outlet />
+		</Document>
+	);
+}
+
+function Document({ children, title }) {
+	return (
+		<html lang='en'>
+			<head>
+				<title>Learn Remix</title>
+			</head>
+			<body>
+				{children}
+				{process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+			</body>
+		</html>
+	);
+}
+
+/* export const meta = () => ({
+	charset: 'utf-8',
+	title: 'New Remix App',
+	viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
+	return (
+		<html lang='en'>
+			<head>
+				<Meta />
 
-        <Links />
-      </head>
+				<Links />
+			</head>
 
-      <body>
-        <Outlet />
+			<body>
+				<Outlet />
 
-        <ScrollRestoration />
+				<ScrollRestoration />
 
-        <Scripts />
+				<Scripts />
 
-        <LiveReload />
-      </body>
-    </html>
-  );
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
+ */
