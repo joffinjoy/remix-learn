82208422a393abecd3b67c2316ba2e7297bb8211
joffinjoy <joffinjoy@gmail.com>
2022-07-31 23:16:43 +0530

    Added Form Handler

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,5 +1,38 @@
+import { redirect } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+
+export const action = async ({ request }) => {
+	const form = await request.formData();
+	console.log(form.get('title'), form.get('body'));
+	return redirect('/posts');
+};
+
 const NewPost = () => {
-	return <div>NewPost</div>;
+	return (
+		<>
+			<div>
+				<h1>New Post</h1>
+				<Link to='/posts' className='btn btn-reverse'>
+					Back
+				</Link>
+			</div>
+			<div>
+				<form method='POST'>
+					<div>
+						<label htmlFor='title'>Title</label>
+						<input type='text' name='title' id='title'></input>
+					</div>
+					<div>
+						<label htmlFor='body'>Post Body</label>
+						<textarea name='body' id='body' />
+					</div>
+					<button className='btn btn-block' type='submit'>
+						Add Post
+					</button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default NewPost;
